ad283e28fb4dae3cb9385b6b3c883669c913f908
Jonntz <monteirojonatas23@gmail.com>
2020-07-13 18:32:47 -0300

    Inserting the indexes

---
+++ projeto2/index.js
@@ -0,0 +1,36 @@
+const cep = document.querySelector("#cep");
+const endereco = document.querySelector("#endereco");
+var oReq = new XMLHttpRequest();
+
+
+function pesquisaCep() {
+    // console.log(cep.value);
+    oReq.open("GET", `https://cep.awesomeapi.com.br/json/${cep.value}`, true);
+    oReq.setRequestHeader("content-type", "application/json");
+
+    oReq.onreadystatechange = function () {
+        if (oReq.readyState == 4 && oReq.status == 404) {
+            alert("Não foi possível fazer a conexão com o banco de dados.");
+
+        } else if (oReq.readyState == 4 && oReq.status == 200) {
+            console.log(this.responseText);
+            const data = JSON.parse(this.responseText);
+
+            const resultElement = document.createElement("div");
+            resultElement.innerHTML = `<br> <p>${data.address}</p>` +
+                `<p>Estado: ${data.state}</p>` +
+                `<p>Cidade: ${data.city}</p>` +
+                `<p>Bairro: ${data.district}</p>` +
+                `<p>DDD: ${data.ddd}</p>`;
+
+            endereco.appendChild(resultElement);
+
+        }
+    };
+
+    oReq.send(JSON.stringify({
+        "cep": cep.value
+    }));
+
+    event.preventDefault();
+}
